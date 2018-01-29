f4bdbe2e2071fc4f3fcbbaac030c5daba9a41138
yamazaki <ma17123@shibaura-it.ac.jp>
2018-01-29 19:42:42 +0900

    centos docker install script add

---
+++ centos/dockerinstall.sh
@@ -0,0 +1,28 @@
+#docker delete
+yes | yum remove docker docker-common docker-selinux docker-engine
+
+#package install 
+yes | yum install -y yum-utils device-mapper-persistent-data lvm2
+
+#add repository
+yes | yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+#yum package index update
+yes | yum makecache fast
+
+#search can install version
+yum list docker-ce.x86_64 --showduplicates | sort -r
+
+#install
+yes | yum install docker-ce-17.12.0.ce-1.el7.centos
+yes | sudo yum install docker-ce
+
+#start
+yes | systemctl start docker
+
+#check
+yes | docker run hello-world
+
+#not needing sudo
+yes | groupadd docker
+yes | usermod -aG docker $USER
